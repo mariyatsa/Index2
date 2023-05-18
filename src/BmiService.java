5d18b051f53e3b7784bd0543447dfb01305f96e9
Mariya <tsapkova_mariya@bk.ru>
2023-05-18 14:42:26 +0500

    Change in formula

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public double calculate(double height, int weight) {
-        return weight / height;
+        return weight / Math.pow(height, 2);
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
-
-
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double height;
-        height = (Math.pow(1.87, 2));
+        double height = 1.87;
         int weight = 98;
         int index = (int) service.calculate(height, weight);
         System.out.println(index);
